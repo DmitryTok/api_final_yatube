6a1ea57784f5f46ba4539353e02f3d537521b900
Dmitry Tok <moon0939110824@gmail.com>
2021-10-25 18:08:33 +0300

    first commit

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,27 @@
-from django.urls import path
+from api.views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+from django.urls import include, path
+from rest_framework import routers
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
+
+router_v1 = routers.DefaultRouter()
+router_v1.register('posts', PostViewSet, basename='post')
+router_v1.register('groups', GroupViewSet, basename='group')
+router_v1.register('follow', FollowViewSet, basename='follow')
+router_v1.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments')
+
 
 urlpatterns = [
+    path('v1/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path(
+        'token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+    path('v1/', include(router_v1.urls)),
 ]
